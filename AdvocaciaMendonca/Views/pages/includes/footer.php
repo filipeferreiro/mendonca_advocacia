17a2799995b95fa4c6bd0640111f8018ac2bd0e1
Filipe Ferreiro Pereira <80466386+filipeferreiro@users.noreply.github.com>
2023-08-03 23:10:05 -0300

    ajustes footer e scrollbar

--- AdvocaciaMendonca/Views/pages/includes/footer.php
+++ AdvocaciaMendonca/Views/pages/includes/footer.php
@@ -3,8 +3,8 @@
                 <div class="footer-top">
                     <img src="<?php echo INCLUDE_PATH_PAGES ?>images/MM.jpg" alt="Logo da Advocacia Mendonça">
                     <div class="footer-info">
-                        <p>emailempresarial@mendoncadvocacia.com</p>
-                        <p>+55 (11) 1111-11111</p>
+                        <p>contato@mendoncadvocacia.com</p>
+                        <p>+55 (11) 9187-82624</p>
                         <p>São Paulo | Brasil</p>
                         <p>Horário de atendimento: segunda a sexta das 8h ás 18h</p>
                     </div><!--footer-info-->
@@ -12,9 +12,9 @@
                 <div class="footer-bottom">
                     <p>Todos os direitos reservados &copy; Marcela Mendonça Advogada</p>
                     <div class="footer-social">
-                        <a href=""><i class="fa-brands fa-square-instagram"></i></a>
-                        <a href=""><i class="fa-brands fa-linkedin"></i></a>
-                        <a href=""><i class="fa-brands fa-square-whatsapp"></i></a>
+                        <a href="https://www.instagram.com/marcelamendonca.adv/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                        <a href="https://www.linkedin.com/in/marcela-pereira-mendon%C3%A7a/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="https://wa.me/5511918782624" target="_blank"><i class="fa-brands fa-square-whatsapp"></i></a>
                     </div><!--footer-social-->
                 </div><!--footer-bottom-->
             </div><!--container-->
